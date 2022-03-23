edeb1d563bc404d05af0c9e27f93d7436f944ded
graham24 <gdholland01@gmail.com>
2022-03-23 12:56:02 -0600

    load recipe on click and page load

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,10 +31,13 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    
     // Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcd09`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -161,4 +164,5 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
