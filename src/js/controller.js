122b4350bcfac39bab21bee057f372424d7a9028
graham24 <gdholland01@gmail.com>
2022-03-28 14:09:37 -0600

    update items rather than re-render

--- src/js/controller.js
+++ src/js/controller.js
@@ -13,9 +13,12 @@ if (module.hot) {
 
 const controlRecipes = async function () {
   try {
-    recipeView.renderSpinner();
     const id = window.location.hash.slice(1);
     if (!id) return;
+    recipeView.renderSpinner();
+
+    // 0) Update results view to mark selected search result
+    resultView.update(model.getSearchResultsPage());
 
     // Loading recipe
     await model.loadRecipe(id);
@@ -60,7 +63,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   //Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
